2691c37374214bde072954fba7f6a1bd89dbd23f
Travis Frazier <48841452+travis-bva@users.noreply.github.com>
2019-06-05 15:35:20 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,92 +1,15 @@
 new Vue({
 	el: '#app',
 	data: {
-		names: [
-			'Nick Annen',
-			'Ryan Apellanes',
-			'Corey Aubuchon',
-			'JJ Bannasch',
-			'Drew Bear',
-			'Grace Benguerel',
-			'Hilary Berryhill',
-			'Brayden Beverage',
-			'Cassie Blench',
-			'Gerald Briones',
-			'Jason Patio',
-			'Corey Burkhart',
-			'Caitlin Byrne',
-			'Marcus Cass',
-			'Chelsey DeBalsi',
-			'Michelle Do',
-			'Chris Dyer',
-			'Taylor Elliot',
-			'Ashley Fejtek',
-			'Alex Fitzpatrick',
-			'Anna Fleming',
-			'Susana Flores',
-			'Lauren Forbes',
-			'Henry Fortier',
-			'Lindsey Frank',
-			'Travis Frazier',
-			'Sean Glass',
-			'Connor Gold',
-			'Ben Greene',
-			'Derek Hanley',
-			'Allyson Helms',
-			'Paul Hughes',
-			'Joshua Jacobs',
-			'Adam Jung',
-			'Joseph Kampman',
-			'Flo Katzenbach',
-			'Sally Kay',
-			'Brendon Keeley',
-			'Kelsey Lancaster',
-			'Andrew Laskey',
-			'James Lawrence',
-			'Daniel Lerman',
-			'Elena Leupp',
-			'Faith Lewis',
-			'Melissa Lopez',
-			'John Lostaglio',
-			'Edgar Martinez',
-			'Ryan Moua',
-			'Lauren Naughton',
-			'Jake Ory',
-			'Joe Pena',
-			'Ian Phillips',
-			'Alex Pierson',
-			'Christina Postma',
-			'Leanne Pratt',
-			'Lindsay Romanelli',
-			'Lindsay Salls',
-			'Riane Sanchez',
-			'David Alvarez',
-			'Brandon Sarver',
-			'Taylor Smith',
-			'Ian Springer',
-			'Brandon Stables',
-			'Jeremy VanDerpluym',
-			'Brian Van Voorst',
-			'Camille Vazquez',
-			'Josh Vervack',
-			'Kyle Villeneuve',
-			'Rachel Waldron',
-			'Courtney Wall',
-			'Sam Webb',
-			'David Williams',
-			'Laura Wusthoff',
-			'Dax Young',
-			'Alicia Zilka',
-			'Dave Myers',
-			'Nina Huang',
-			'Vladamir Skok'
-		],
+		url:
+			'https://script.google.com/macros/s/AKfycbzfaP-qXdHXAHEFHKPDH6MvNlPGI_JVjxfGrYrL66u3sGvR-rd4/exec',
 		personOne: '',
 		personTwo: '',
 		personThree: '',
 		personFour: '',
 		personFive: '',
-		personSix: ''
+		personSix: '',
+		names: []
 	},
 	methods: {
 		generateNames: function() {
@@ -103,12 +26,23 @@ new Vue({
 				return input;
 			};
 			this.names.shuffle();
-			this.personOne = this.names[0];
-			this.personTwo = this.names[1];
-			this.personThree = this.names[2];
-			this.personFour = this.names[3];
-			this.personFive = this.names[4];
-			this.personSix = this.names[5];
+			this.personOne = this.names[0].id;
+			this.personTwo = this.names[1].id;
+			this.personThree = this.names[2].id;
+			this.personFour = this.names[3].id;
+			this.personFive = this.names[4].id;
+			this.personSix = this.names[5].id;
 		}
+	},
+	created() {
+		axios
+			.get(this.url)
+			.then(response => {
+				// JSON responses are automatically parsed.
+				this.names = response.data.user;
+			})
+			.catch(e => {
+				this.errors.push(e);
+			});
 	}
 });
